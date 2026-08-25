1cbe8b07dbb14930a4f4072d2b35a9bb17481f86
Anggito09 <114100817+Anggito09@users.noreply.github.com>
2026-08-25 14:52:14 +0700

    fix: restore stable app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./Home";
 import "./styles.css";
-import { installUiFixes } from "./uiFixes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Home />
   </StrictMode>,
 );
-
-installUiFixes();
